27a56b30139f60b0cae08f14051f8d1f33bedc84
pablosimkievich <105614947+pablosimkievich@users.noreply.github.com>
2023-03-13 10:00:46 -0300

    Update Ingenio.jsx
    update api url

--- src/components/productTables/Ingenio.jsx
+++ src/components/productTables/Ingenio.jsx
@@ -7,7 +7,7 @@ import {useEffect, useState} from 'react';
 function Ingenio(){
     const [dataIngenio, setdataIngenio] = useState([]);
     const products =  async () => {
-        const json = await axios ("http://localhost:3001/api/products");//pego a mi api
+        const json = await axios ("https://rayuela.onrender.com/api/products");//pego a mi api
             
         setdataIngenio(json.data.juguetesXCategoria.ingenio);
         }
@@ -47,7 +47,7 @@ function Ingenio(){
                                     Age: e.age, 
                                     Categories: e.category,
                                     Description: e.description,
-                                    Edit: `http://localhost:3001/edit/${e.id}`,
+                                    Edit: `https://rayuela.onrender.com/edit/${e.id}`,
                                   
                                 }
                                
